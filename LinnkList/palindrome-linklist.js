4441526318846fd8bf75ae2f4cfbd49611fe2d63
Sarvesh207 <sarveshgaynar@gmail.com>
2026-05-15 22:54:35 +0530

    refactor isPalindrome function for improved readability and consistency

--- LinnkList/palindrome-linklist.js
+++ LinnkList/palindrome-linklist.js
@@ -10,36 +10,34 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-  // Find The Middle
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  // revers the link from the middle
-
-  let prev = null;
-  let curr = slow;
-
-  while (curr) {
-    let temp = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = temp;
-  }
-
-  let firstList = head;
-  let secondList = prev;
-
-  while (secondList) {
-    if (secondList.val !== firstList.val) {
-      return false;
+    // Find the middle 
+    let slow = head;
+    let fast = head
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    secondList = secondList.next;
-    firstList = firstList.next;
-  }
-  return true;
+    // Reverse the Linklist from middle pointer 
+    let prev = null;
+    let curr = slow;
+
+    while (curr !== null) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp
+    }
+    // compare both first half and second half 
+    let firstList = head;
+    let secondList = prev;
+
+    while (secondList !== null) {
+        if (secondList.val !== firstList.val) {
+            return false;
+        }
+        secondList = secondList.next;
+        firstList = firstList.next;
+    }
+
+    return true;
 };
