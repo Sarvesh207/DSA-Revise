51135bcbcc7a472bfa6bebf0cc037498261bf528
Sarvesh207 <sarveshgaynar@gmail.com>
2026-05-24 20:43:54 +0530

    js practice

---
+++ js-hard-questions/Javascript-revise.js
@@ -0,0 +1,15 @@
+// console.log(null == undefined);
+// console.log(undefined == null);
+// console.log(null === undefined);
+// console.log(undefined === null);
+
+// console.log([] == "");
+// console.log([] == 0);
+// console.log({} == 0);
+
+//  Hoisting
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+// let a = 10;
