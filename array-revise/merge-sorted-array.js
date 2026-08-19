4b5947c74c079840c88f1976e18008c5dc962f13
Sarvesh207 <sarveshgaynar@gmail.com>
2026-08-19 13:17:37 +0530

    better approch to solve the merge sorted array

--- array-revise/merge-sorted-array.js
+++ array-revise/merge-sorted-array.js
@@ -1,7 +1,7 @@
 // Brute Force Approchh
 //* Copy the num2 into num1 and then sort it
 //  */
-function mergeArray(num1, num2) {
+function mergeArrayWithBruteForce(num1, num2) {
   let n = num1.length;
   let m = num2.length;
 
@@ -15,8 +15,31 @@ function mergeArray(num1, num2) {
   num1.sort((a, b) => a - b);
 }
 
+function mergeArray(num1, m, num2, n) {
+  let num1copy = num1.splice(0, n);
+  let p1 = 0;
+  let p2 = 0;
+  let i = 0;
+  while (p1 < m && p2 < n) {
+    if (num1copy[p1] < num2[p2]) {
+      num1[i++] = num1copy[p1++];
+    } else {
+      num1[i++] = num2[p2++];
+    }
+  }
+
+  while (p1 < m) {
+    num1[i++] = num1copy[p1++];
+  }
+  while (p2 < n) {
+    num1[i++] = num2[p2++];
+  }
+}
+
 const num1 = [1, 2, 3, 0, 0, 0];
 const num2 = [2, 5, 6];
+let m = num1.length;
+let n = num2.length;
 
-mergeArray(num1, num2);
+mergeArray(num1, 3, num2, n);
 console.log(num1);
