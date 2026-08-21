e8486c7d690f19f44d551de0eb780e80957a50ab
Sarvesh207 <sarveshgaynar@gmail.com>
2026-08-21 08:10:33 +0530

    revise single number and missing number problem

---
+++ array-revise/single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let unique = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    unique = unique ^ nums[i];
+  }
+
+  return unique;
+};
