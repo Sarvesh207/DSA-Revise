2e7303b1e0c3736ef6e87328ce243c69d4601349
Sarvesh207 <sarveshgaynar@gmail.com>
2026-08-30 23:04:15 +0530

    add link list

---
+++ linklist-revise/remove-linklist-element.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  let sentinialNode = new ListNode();
+  sentinialNode.next = head;
+  let curr = sentinialNode;
+  while (curr && curr.next) {
+    if (curr.next.val == val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return sentinialNode.next;
+};
