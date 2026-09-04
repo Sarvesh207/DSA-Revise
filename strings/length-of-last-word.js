f781775010a7865a110952201b99fc038007ad73
Sarvesh207 <sarveshgaynar@gmail.com>
2026-09-04 08:50:43 +0530

    better approch and single loop

--- strings/length-of-last-word.js
+++ strings/length-of-last-word.js
@@ -4,3 +4,36 @@ function lengthOfLastWord_bruteForce(s) {
 
   return s[s.length - 1].length;
 }
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWordBetter = function (s) {
+
+    let n = s.length - 1;
+
+    while (n >= 0) {
+
+        if (s[n] == " ") {
+            --n;
+        } else {
+            break;
+        }
+    }
+
+    let count = 0;
+
+    while (n >= 0) {
+
+        if (s[n] !== " ") {
+            --n;
+            ++count;
+        } else {
+            break
+        }
+    }
+
+    return count;
+};
